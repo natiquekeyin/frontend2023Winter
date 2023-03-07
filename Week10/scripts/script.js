9d0a502706b80efc25d52bf10ff074aacb703c29
Noman Atique <noman.atique@keyin.com>
2023-03-07 11:22:08 -0500

    march7

--- Week10/scripts/script.js
+++ Week10/scripts/script.js
@@ -12,12 +12,19 @@
 //   };
 // };
 
-window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("DOMContentLoaded", function (evt) {
   console.log("Window laoded");
   let dog = document.querySelector("#dog");
 
-  dog.addEventListener("click", function () {
+  dog.addEventListener("click", function (evt) {
+    console.log("dog clicked");
+    // remove the The Dog heading...
+    // document.querySelector("#p1").remove();
+    let p1 = document.querySelector("#p1");
+    p1.remove();
+
     dog.src = nextDogUrl(); //nextDogUrl will bring the next url from dogs.js urls array
+    evt.stopPropagation(); //dont propagate the event to my parent...
   });
 
   let currentDogUrl = 0; //to keep track of current dog url
@@ -36,19 +43,53 @@ window.addEventListener("DOMContentLoaded", function () {
   let more = document.querySelector("#more");
 
   more.addEventListener("click", function () {
+    more.remove();
     let ol = document.createElement("ol");
-
+    // creates a new <ol></ol>
     dogs.forEach(function (url) {
       let li = document.createElement("li");
       let a = document.createElement("a");
       a.href = url;
       a.innerHTML = url;
+      // <a href={url}> {url}</a>
       li.appendChild(a);
+      // <li><a href={url}>{url}</a></li>
       ol.appendChild(li);
     });
 
-    // append this ul in dog-list of html document
+    // append this ol which is cooked up by forEach in dog-list of html document
 
     document.querySelector("#dog-list").appendChild(ol);
   });
+
+  // Show the mouse position on the screen when user moves the mouse...show the X and Y value of mouse position on the document ....
+
+  let body = document.body;
+
+  body.addEventListener("mousemove", function (evt) {
+    console.log("body clicked");
+    // get the xPos,yPos elements in teh html, and write something inside them.
+    // to access the x and y position of mouse click we can use clientX and clientY of evt...
+    document.querySelector("#xPos").innerText = evt.clientX;
+    document.querySelector("#yPos").innerText = evt.clientY;
+  });
+  // When we prouce an event to an element, it is propagated to the parent...evt.stopPropagation();
+
+  // Timer Functions...Are browser or timer dependent functions...
+
+  // setTimeout(function, timeinmilliseconds);
+  // when the time is elapsed RUN THE FUNCTION...
+
+  // setTimeout(function () {
+  //   alert("3 seconds passed");
+  // }, 3000);
+  // It runs only once after teh elapsed time
+  // setTimeout(function () {
+  //   // dog.remove();
+  //   dog.src = nextDogUrl();
+  // }, 4000);
+
+  setInterval(function () {
+    dog.src = nextDogUrl();
+  }, 3000);
 });
