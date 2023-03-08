84494c367c6ec97bc6c00f8bd6d71c28daa7dab1
Noman Atique <noman.atique@keyin.com>
2023-03-08 08:04:29 -0500

    march8

--- Week7/scripts/feb16.js
+++ Week7/scripts/feb16.js
@@ -23,7 +23,7 @@ function addBookToList(t, a, i) {
   } else {
     let row = document.createElement("tr");
 
-    row.innerHTML = `<td>${t}</td><td>${a}</td><td>${i}</td <td><a href="#">DELETE<a></td>`;
+    row.innerHTML = `<td>${t}</td><td>${a}</td><td>${i}</td> <td><a href="#" class="delete">DELETE</a></td>`;
     document.querySelector("#list").appendChild(row);
 
     showAlert("Book added successfully!!!!", "success");
@@ -49,3 +49,25 @@ function showAlert(m, c) {
 }
 
 // We will start from here... first we will work on showAlert()... and then we will continute with this project... i) like deleting the book from the list ii) displaying a color popup[done this but will repeat...]... iii) permanently storing the books in browser memory iv) Apply core object orirented programming to this project.. (classes and objects...)
+
+// Step 1 .. i have to apply an event listener to the table...
+// console.log(evt.target); //the element on which you have clicked...
+
+document.querySelector("#book-list").addEventListener("click", function (evt) {
+  removeBook(evt.target);
+});
+
+function removeBook(elemToDelete) {
+  if (elemToDelete.className === "delete") {
+    elemToDelete.parentElement.parentElement.remove();
+    showAlert("Book removed successfully", "success");
+  } else {
+    showAlert("Wrong area .. click on DELETE instead", "error");
+  }
+}
+
+// <tr><td>title</td><td>author</td><td>isbn</td><td><a href="#" class="delete">DELETE</a></td></tr>
+
+// iii) Apply core object orirented programming to this project.. (classes and objects...)
+
+// iv) permanently storing the books in browser memory
