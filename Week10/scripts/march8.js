bca2a837e5011888ad6796bf3f37bbd4d59424f2
Noman Atique <noman.atique@keyin.com>
2023-03-08 12:24:25 -0500

    march8_2

--- Week10/scripts/march8.js
+++ Week10/scripts/march8.js
@@ -57,14 +57,134 @@ R C
 
 */
 
-var campuses = [
-  [43.123, -89.333],
-  [46.456, -69.2222],
-  [42.3332, -90.5555],
-];
+// var campuses = [
+//   [43.123, -89.333],
+//   [46.456, -69.2222],
+//   [42.3332, -90.5555],
+// ];
 
-for (var i = 0; i < 3; i++) {
-  for (var j = 0; j < 2; j++) {
-    console.log(campuses[i][j]);
-  }
+// for (var i = 0; i < 3; i++) {
+//   for (var j = 0; j < 2; j++) {
+//     console.log(campuses[i][j]);
+//   }
+// }
+
+// VERY IMPORTANT HERE...ARRAY OF OBJECTS..
+// var campuses = [
+//   { lat: 43.5333, lng: -29.5433 },
+//   { lat: 41.5333, lng: -39.5433 },
+//   { lat: 42.5333, lng: -89.5433 },
+//   { lat: 45.5333, lng: -90.5433 },
+//   { lat: 48.5333, lng: -79.5433 },
+// ];
+
+// var campuses = [
+//   { lat: 43.5333, lng: -29.5433 },
+//   { lat: 41.5333, lng: -39.5433 },
+//   { lat: 42.5333, lng: -89.5433 },
+//   { lat: 45.5333, lng: -90.5433 },
+//   { lat: 48.5333, lng: -79.5433 },
+// ];
+
+// console.log(campuses[1]);
+
+// when we are reading from object.. this is called get operation...getters
+// when we are setting a value in object.. this is called set opeation... setters..
+
+// console.log(user.email);
+// user.firstName = "Bob";
+// user.loggedIn = true;
+// console.log(user);
+
+// var user = {
+//   firstName: "Alex",
+//   lastName: "Smith",
+//   email: "alex@gmail.com",
+//   loggedIn: false,
+//   hobbies: ["reading", "sports", "cycling"],
+//   prefs: { color: "Red", mode: "Audi" },
+//   logIn: function () {
+//     this.loggedIn = true;
+//   },
+//   logOut: function () {
+//     this.loggedIn = false;
+//   },
+// };
+
+// // An object can contain ANY type of fields inside itself
+
+// console.log(user);
+// // user.logIn();
+// // console.log(user);
+// // user.logOut();
+// // console.log(user);
+
+// var user1 = {
+//   firstName: "Bob",
+//   lastName: "Smith",
+//   email: "bob@gmail.com",
+//   loggedIn: true,
+//   hobbies: ["something", "social", "cycling"],
+//   prefs: { color: "Yellow", mode: "Vovo" },
+//   logIn: function () {
+//     this.loggedIn = true;
+//   },
+//   logOut: function () {
+//     this.loggedIn = false;
+//   },
+// };
+
+// console.log(user1);
+
+// Constructor Function. ..
+// The constructor function starts with a capital letter...
+
+// function User(f, l, e) {
+//   this.firstName = f;
+//   this.lastName = l;
+//   this.email = e;
+//   this.loggedIn = true;
+//   this.hobbies = ["something", "social", "cycling"];
+//   this.prefs = { color: "Yellow", mode: "Vovo" };
+//   this.logIn = function () {
+//     this.loggedIn = true;
+//   };
+//   this.logOut = function () {
+//     this.loggedIn = false;
+//   };
+// }
+
+// var user1 = new User("Alan", "Smith", "alan@gmail.com");
+// var user2 = new User("Christina", "Lee", "chris@gmail.com");
+// console.log(user2);
+// user2.prefs = { color: "Red", mode: "Audi" };
+// console.log(user2);
+
+function User(f, l, e) {
+  this.firstName = f;
+  this.lastName = l;
+  this.email = e;
+  this.loggedIn = true;
+  this.hobbies = ["something", "social", "cycling"];
 }
+
+User.prototype.anything = "hello";
+User.prototype.prefs = { color: "Yellow", mode: "Vovo" };
+User.prototype.logIn = function () {
+  this.loggedIn = true;
+};
+
+User.prototype.logOut = function () {
+  this.loggedIn = false;
+};
+
+var user1 = new User("Alan", "Smith", "alan@gmail.com");
+console.log(user1.prefs);
+console.log(user1.anything);
+// user1.logOut();
+// console.log(user1);
+// var user2 = new User("Bob", "Smith", "bob@gmail.com");
+// var user1000 = new User("xxxtx", "xxttx", "xxxtt@gmail.com");
+// user1.logOut(); // its very less likely or occasional that we may call logout and login functions...
+
+// JavaScript defines something called "Prototype".. .Prototypal Inheritance.... where we define a prototype and store all commone features there.. and they are n ot copied with every object creation....
