2b694018280afad64de0455d8cbd6ac0bb89a1e2
Noman Atique <noman.atique@keyin.com>
2023-03-17 08:56:01 -0400

    secondreview

--- Review_March17/scripts/script.js
+++ Review_March17/scripts/script.js
@@ -15,6 +15,14 @@
 //   "#output"
 // ).innerHTML = `The book <strong> ${objBook2.title}</strong> is written by <strong> ${objBook2.author}</strong>. Thanks!`;
 
+//   if (parts[1] === "Noman") {
+//     document.querySelector("#output2").innerHTML = `Noman Atique`;
+//   } else if (parts[1] === "Jonah") {
+//     document.querySelector("#output2").innerHTML = `Jonah Greening`;
+//   } else if (parts[1] === "Tyler") {
+//     document.querySelector("#output2").innerHTML = `Tyler Stuckless`;
+//   }
+
 function Book(t, a) {
   this.title = t;
   this.author = a;
@@ -31,6 +39,34 @@ form.addEventListener("submit", function (evt) {
   document.querySelector(
     "#output"
   ).innerHTML = `The book <strong><em>${objBook.title}</em></strong> is written by ${objBook.author}`;
+
+  evt.preventDefault();
+});
+
+let button1 = document.querySelector("#btn1");
+
+button1.addEventListener("click", function (evt) {
+  let message = document.querySelector("#str").value;
+  let parts = message.split(".");
+
+  switch (parts[1]) {
+    case "html":
+      document.querySelector("#output2").innerHTML = `Noman Atique`;
+      break;
+
+    case "Jonah":
+      document.querySelector("#output2").innerHTML = `Jonah Greening`;
+      break;
+
+    case "Tyler":
+      document.querySelector("#output2").innerHTML = `Tyler Stuckless`;
+      break;
+    default:
+      document.querySelector(
+        "#output2"
+      ).innerHTML = `Sorry you are not present today!`;
+      break;
+  }
 
   evt.preventDefault();
 });
